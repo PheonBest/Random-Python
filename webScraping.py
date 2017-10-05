3ce4c6ca0cb450bf2068acaae7e68a287d4cf266
iErcann <ercansarpcan@hotmail.FR>
2017-10-05 21:03:06 +0200

    removed my password

--- webScraping.py
+++ webScraping.py
@@ -1,26 +1,58 @@
 from bs4 import BeautifulSoup
 import requests
-def getAllPost(jvc_topic_url):
-	page = requests.get(jvc_topic_url)
+
+page_number = 1; 
+url = "http://www.jeuxvideo.com/forums/42-51-49873942-" + str(page_number) + "-0-1-0-les-depressifs-on-se-regroupe-ici.htm"
+page = requests.get(url)
+contents = page.content
+soup = BeautifulSoup(contents, 'html.parser')
+
+
+def refreshUrl():
+	global url
+	global page
+	global contents
+	global soup
+	url = "http://www.jeuxvideo.com/forums/42-51-49873942-" + str(page_number) + "-0-1-0-les-depressifs-on-se-regroupe-ici.htm"
+	page = requests.get(url)
 	contents = page.content
 	soup = BeautifulSoup(contents, 'html.parser')
+
+def getAllPost():
 	postList = soup.findAll("div", { "class" : "inner-head-content" });
 	return postList
  
 
+def printAllPost():
+	allPost = getAllPost()
+	for i in range(0, len(allPost)): 
+		currentPage = soup.findAll("span", { "class" : "page-active" })[0].text;
+		textFixed = allPost[i].text
+		textFixed = textFixed.replace("MP", " ")
+		textToInsert = "Page " + str(currentPage) +" "+ "Post N°" + str(i)
+		textFixed = list(textFixed)
+		for a in range(0, len(textToInsert)):
+			 textFixed[a] = textToInsert[a]
 
-allPost = getAllPost("http://www.jeuxvideo.com/forums/42-51-53428771-1-0-1-0-les-punaises-de-lit-cet-enfer.htm")
+		print("".join(textFixed));
+		
+		for d in range(0, 2):
+			print("\n")
 
-for i in range(0, len(allPost)): 
-	#print("------- Post N°" + str(i) + "-------")
-	textFixed = allPost[i].text
-	textFixed = textFixed.replace("MP", " ")
-	textToInsert = "Post N°" + str(i)
-	textFixed = list(textFixed)
-	for a in range(0, len(textToInsert)):
-		 textFixed[a] = textToInsert[a]
 
-	print("".join(textFixed));
-	
-	for d in range(0, 2):
-		print("\n")
+printAllPost();
+
+'''
+while page_number < 5:
+ 	page_number += 1;
+ 	refreshUrl();
+ 	printAllPost();
+'''
+
+command = input();
+print(command)
+
+if command == "np": # next page
+	page_number += 1;
+	refreshUrl();
+	printAllPost();
