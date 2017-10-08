bbb2a46556598f9de07b377bea8ee09b2a4121a6
iErcann <ercansarpcan@hotmail.FR>
2017-10-08 16:46:20 +0200

    red detect

--- oui.py
+++ oui.py
@@ -1,9 +1,18 @@
-import pynput 
 import pyscreenshot as ImageGrab
-    
+from pynput.mouse import Button, Controller
 
+mouse = Controller()
 
+   
 if __name__ == "__main__":
-    # part of the screen
-    im=ImageGrab.grab(bbox=(0,0,1920,1080)) # X1,Y1,X2,Y2
-    im.show() 
+    im=ImageGrab.grab(bbox=(100,100,600,600)) # X1,Y1,X2,Y2
+    im.show()
+    for i in range(0, 500):
+    	for j in range(0, 500):
+    		r,g,b = im.getpixel((i,j))
+    		print(r,g,b)
+    		if (r == 255 and g == 0 and b == 0):
+    			mouse.position = (100+i, 100+j)
+    			mouse.press(Button.left)
+				mouse.release(Button.left)
+    #im.show() 
