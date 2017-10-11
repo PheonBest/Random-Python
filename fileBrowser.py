48aa2892966f01f22cf94c692652490bb1be9b50
iErcann <ercansarpcan@hotmail.FR>
2017-10-11 20:53:32 +0200

    Add files via upload

--- fileBrowser.py
+++ fileBrowser.py
@@ -9,15 +9,15 @@ fenetre = Tk()
 dirs = os.listdir()
 print(dirs)
 fenetre.title(str(os.getcwd()))
-def printThis(strc):
-	print(strc)
 
-def readFile(file):
-	f = open(file, "w")
-	return str(f)
-for i in range(0, len(dirs)):
-	file = Button(fenetre, text=dirs[i])
-	Button(fenetre, text="X", command=lambda i=i : print(readFile(dirs[i]))).grid(row=2, column=i)
-	file.grid(row=1, column=i)
+def removeFile(file):
+	os.remove(file)
+	refresh();
 
+def refresh():
+	for i in range(0, len(dirs)):
+		file = Button(fenetre, text=dirs[i])
+		Button(fenetre, text="X", command=lambda i=i : removeFile(dirs[i]), fg="red").grid(row=2, column=i)
+		file.grid(row=1, column=i)
+refresh()
 fenetre.mainloop()
