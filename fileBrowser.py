b242ee71f73a6b4a84aab33b6bed6ec4e005e9f2
iErcann <ercansarpcan@hotmail.FR>
2017-10-11 19:27:42 +0200

    ui

--- fileBrowser.py
+++ fileBrowser.py
@@ -9,11 +9,15 @@ fenetre = Tk()
 dirs = os.listdir()
 print(dirs)
 fenetre.title(str(os.getcwd()))
+def printThis(strc):
+	print(strc)
+
+def readFile(file):
+	f = open(file, "w")
+	return str(f)
 for i in range(0, len(dirs)):
 	file = Button(fenetre, text=dirs[i])
-	delete = Button(fenetre, text="X", command=lambda : os.remove(dirs[i]))
-
+	Button(fenetre, text="X", command=lambda i=i : print(readFile(dirs[i]))).grid(row=2, column=i)
 	file.grid(row=1, column=i)
-	delete.grid(row=2, column=i)
 
 fenetre.mainloop()
