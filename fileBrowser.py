c1bd5acd5fc29fe57b0156f573bcc5e8596476ff
iErcann <ercansarpcan@hotmail.FR>
2017-10-12 20:19:40 +0200

    Add files via upload

--- fileBrowser.py
+++ fileBrowser.py
@@ -10,14 +10,22 @@ dirs = os.listdir()
 print(dirs)
 fenetre.title(str(os.getcwd()))
 
+def deleteWidget():
+	widgets = fenetre.winfo_children()
+	for j in range(0, len(widgets)):
+		widgets[j].destroy() 	
+	time.sleep(1)
+	#refresh()
+
 def removeFile(file):
 	os.remove(file)
-	refresh();
+	deleteWidget()
 
 def refresh():
 	for i in range(0, len(dirs)):
-		file = Button(fenetre, text=dirs[i])
+		file = Button(fenetre, text=dirs[i], command=lambda : print("ok")).grid(row=1, column=i)
 		Button(fenetre, text="X", command=lambda i=i : removeFile(dirs[i]), fg="red").grid(row=2, column=i)
-		file.grid(row=1, column=i)
+ 
 refresh()
 fenetre.mainloop()
+ 
